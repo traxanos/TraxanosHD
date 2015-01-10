050bf6d457e347f4718f901cd0e6d8859e23e385
Marco Scholl <develop@marco-scholl.de>
2015-01-10 23:20:13 +0100

    add epg key for timerlist

--- screens/TimerEditList.rb
+++ screens/TimerEditList.rb
@@ -70,4 +70,20 @@ TraxanosHD::Screen.new(xml, :TimerEditList, "Timer Editor") do
   colorbar do
     colorbar_buttons1(true, true, true, true)
   end
+
+  box do
+    position TraxanosHD.border+TraxanosHD.left_width+TraxanosHD.spacer, 645
+    size TraxanosHD.right_width-(TraxanosHD.spacer*2), 30
+
+    label do
+      position -72
+      size 72, 30
+      text "EPG"
+      font "SegoeUI", 20
+      nowrap 1
+      orientation :center, :center
+      background :foregroundAccent
+      foreground :backgroundAccent
+    end
+  end
 end
