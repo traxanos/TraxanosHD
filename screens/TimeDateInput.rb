e7b076ff1a674b56b9c8b267104861705147f25b
Marco Scholl <develop@marco-scholl.de>
2015-01-12 23:30:34 +0100

    add TimeDateInput

---
+++ screens/TimeDateInput.rb
@@ -0,0 +1,53 @@
+TraxanosHD::Screen.new(xml, :TimeDateInput, "Time/Date Input") do
+  position :center, :center, 997
+  size 378 + (TraxanosHD.spacer*2), 150
+  label do
+
+    widget do
+      position TraxanosHD.spacer, TraxanosHD.spacer
+      size -(TraxanosHD.spacer*2), 30*3
+      itemheight 30
+      name "config"
+      transparent 1
+      scrollbars
+    end
+
+    box do
+      position 0, 120-6
+      size nil, 6 + 30
+
+      label do
+        position 18, 0
+        size 180, 3
+        background :red
+      end
+
+      label do
+        position 216, 0
+        size 180, 3
+        background :green
+      end
+
+      widget do
+        position 18-9
+        size 198, 30
+        name "canceltext"
+        font "Regular", 16
+        orientation :center, :center
+        nowrap 1
+        transparent 1
+      end
+
+      widget do
+        position 216-9
+        size 198, 30
+        name "oktext"
+        font "Regular", 16
+        orientation :center, :center
+        nowrap 1
+        transparent 1
+      end
+
+    end
+  end
+end
